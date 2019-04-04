af3296b001f75d45afbe5cd3fc03b0964bdbd4c3
github username <github email address>
2019-04-04 16:51:17 +0100

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import data from '../data'
+
+class App extends React.Component {
+  render() {
+    return (
+     <div>App</div>
+    );
+  }
+}
+
+export default App;
